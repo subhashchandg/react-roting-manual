e56021e279d77918d6cc679756050fbbe156f4ce
Subhash Chand <chandu.5a4@gmail.com>
2020-07-11 18:18:08 +0530

    throttling implemented

--- src/components/search/search.jsx
+++ src/components/search/search.jsx
@@ -1,5 +1,44 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 export default ()=>{
-    return <h1>I AM a Search</h1>
+
+    const [text , setText ] = useState('programming');
+    const [debounceText , setDebounceText ] = useState(text);
+    const [searchResults, setSearchResults ] = useState([]);
+
+    useEffect(()=>{
+        const timeOutId = setTimeout(()=>{
+            setDebounceText(text);
+        },1000);
+
+        return ()=>{
+            clearTimeout(timeOutId);
+        }
+    },[text]);
+
+    useEffect(()=>{
+
+    fetch(`https://en.wikipedia.org/w/api.php?action=query&list=search&origin=*&format=json&srsearch=${debounceText}`)
+    .then(response => response.json())
+    .then(json => setSearchResults(json.query.search))
+
+    },[debounceText]);
+
+    const onclickHandler =({target})=>{
+        setText(target.value);
+    }
+    return (
+        <>
+        <input onInput={onclickHandler} type="text"/>
+        <div>
+            <div>Search Results below:</div>
+    { searchResults.map(el=>(
+        <div>
+            <div>{el.title}</div>
+            <span dangerouslySetInnerHTML={{__html:el.snippet}}></span>
+        </div>))
+    }
+        </div>
+        </>
+    )
 }
